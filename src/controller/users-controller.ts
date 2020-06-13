98787e7eba7041b1ec7ac4a0de2e2a67dfdc1b4d
Binarysearch <desenlace.net@gmail.com>
2020-06-13 19:01:30 +0200

    login y login with token en post

--- src/controller/users-controller.ts
+++ src/controller/users-controller.ts
@@ -4,13 +4,40 @@ import { Observable, of, forkJoin, throwError } from "rxjs";
 import { map, mergeMap } from "rxjs/operators";
 import { UserCredentialsDto } from "../interface/dtos/user-credentials-dto";
 import * as uuid from "uuid";
+import { SessionsDao } from "../dao/sessions-dao";
 
 @Controller
 export class UsersController {
 
     constructor(
-        private usersDao: UsersDao
+        private usersDao: UsersDao,
+        private sessionsDao: SessionsDao
     ) { }
+
+    @Post('login-with-token')
+    public loginWithToken(credentials: { authToken: string; }): Observable<string> {
+        return this.sessionsDao.getByToken(credentials.authToken).pipe(
+            map((session) => {
+                return session.userId;
+            })
+        );
+    }
+
+    @Post('login')
+    public login(credentials: { username: string; password: string; authToken: string; }): Observable<string> {
+        return new Observable((obs) => {
+            this.usersDao.getByUsername(credentials.username).subscribe(
+                user => {
+                    if (user && user.password === credentials.password) {
+                        obs.next(user.id);
+                        obs.complete();
+                    } else {
+                        obs.error({ httpStatusCode: 401, description: 'Invalid credentials' });
+                    }
+                }
+            );
+        });
+    }
 
     @Post('register')
     public register(credentials: UserCredentialsDto): Observable<string> {
